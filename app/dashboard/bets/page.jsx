573481cee5f15dcd1c1bdc47f1f9650e69726af8
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-10-04 07:52:19 +0700

    Update page.jsx

--- app/dashboard/bets/page.jsx
+++ app/dashboard/bets/page.jsx
@@ -1,1 +1,76 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import { fetchBets } from "@/app/lib/data";
+import { deleteBet } from "@/app/lib/actions";
 
+const BetPage = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
+  const { count, bets } = await fetchBets(q, page);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/bets/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {bets.map((bet) => (
+            <tr key={bet.id}>
+              <td>
+                <div className={styles.product}>
+                  <Image
+                    src={product.img || "/noproduct.jpg"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.productImage}
+                  />
+                  {bet.title}
+                </div>
+              </td>
+              <td>{bet.desc}</td>
+              <td>${bet.price}</td>
+              <td>{bet.createdAt?.toString().slice(4, 16)}</td>
+              <td>{bet.stock}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/bets/${product.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form action={deleteBet}>
+                    <input type="hidden" name="id" value={product.id} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination count={count} />
+    </div>
+  );
+};
+
+export default BetPage;
